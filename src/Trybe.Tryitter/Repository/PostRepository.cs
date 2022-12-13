636a4ca4222fb27a9b223a0885a756230b723eb9
matheus <mathe@PAOCASTATION>
2022-12-12 23:36:37 -0300

    feat: posts crud

---
+++ src/Trybe.Tryitter/Repository/PostRepository.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Extensions.Hosting;
+using Trybe.Tryitter.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Trybe.Tryitter.Repository
+{
+    public class PostRepository : IPostRepository
+    {
+        private readonly IMyContext _context;
+
+        public PostRepository(IMyContext context)
+        {
+            _context = context;
+        }
+
+        public Post? AddPost(Post post)
+        {
+            _context.Posts.Add(post);
+            _context.SaveChanges();
+            return _context.Posts.Find(post?.PostId);
+        }
+
+        public Post? GetPostById(int postId)
+        {
+            return _context.Posts.FirstOrDefault(x => x.PostId == postId);           
+        }
+
+        public IEnumerable<Post> GetAllPosts() 
+        {
+            return _context.Posts.ToList();
+        }
+
+        public void UpdatePost(Post post)
+        {
+            _context.Posts.Entry(post).State = EntityState.Modified;
+            _context.SaveChanges();
+        }
+
+        public bool DeletePost(int postId)
+        {
+            var current = _context.Posts.Find(postId);
+            if (current == null) 
+            {
+                return false;
+            }
+            _context.Posts.Remove(current);
+            _context.SaveChanges();
+            return true;
+        }
+    }
+}
