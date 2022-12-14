20a3fefb6e5375065d30c1bddc178c9c8d440e78
Patrick <patrickbastosc@gmail.com>
2022-12-13 21:25:06 -0300

    feat: Created IUser repository

--- src/Trybe.Tryitter/Repository/IUserRepository.cs
+++ src/Trybe.Tryitter/Repository/IUserRepository.cs
@@ -5,6 +5,10 @@ namespace Trybe.Tryitter.Repository
     public interface IUserRepository
     {
         bool AddUser (User user);
-        User GetUser (User user);
+        User GetUserAuth (User user);
+        public User? GetUserById(int id);
+        IEnumerable<UserDTO> GetUsers();
+        public void UpdateUser(User user);
+        public bool DeleteUser(int id);
     }
 }
