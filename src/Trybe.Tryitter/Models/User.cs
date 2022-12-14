73743ecaf21af9bcae1e5a0f0f0e6ef1fdc61841
Patrick <patrickbastosc@gmail.com>
2022-12-13 21:24:41 -0300

    feat: Created UserDTO and included Modulo field

--- src/Trybe.Tryitter/Models/User.cs
+++ src/Trybe.Tryitter/Models/User.cs
@@ -1,12 +1,24 @@
 ﻿namespace Trybe.Tryitter.Models
 {
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.ComponentModel.DataAnnotations;
     public class User
     {
+        [Key]
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
         public string Status { get; set; }
+        public string Modulo { get; set; }
+    }
 
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+        public string Modulo { get; set; }
     }
 }
