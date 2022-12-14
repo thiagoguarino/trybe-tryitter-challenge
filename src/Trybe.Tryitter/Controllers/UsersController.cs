a583f93932f90602ccb067ea34cc895338b37db0
Patrick <patrickbastosc@gmail.com>
2022-12-13 21:24:05 -0300

    feat: Created add, update, get and delete users controllers

--- src/Trybe.Tryitter/Controllers/UsersController.cs
+++ src/Trybe.Tryitter/Controllers/UsersController.cs
@@ -4,8 +4,8 @@ using Trybe.Tryitter.Repository;
 
 namespace Trybe.Tryitter.Controllers
 {
-    [ApiController]
-    [Route("trybe-tryitter")]
+    
+    [Route("trybe-tryitter/users")]
     public class UsersController : ControllerBase
     {
         private readonly IUserRepository _usersRepository;
@@ -15,8 +15,18 @@ namespace Trybe.Tryitter.Controllers
             _usersRepository = usersRepository;
         }
 
-        [HttpPost]
-        [Route("add-user")]
+        [HttpGet("get-user-by-id/{id}")]
+        public IActionResult GetUserById(int id) 
+        {
+            var result = _usersRepository.GetUserById(id);
+            if (result == null)
+            {
+                return NotFound();
+            }
+            return Ok(result);
+        }
+
+        [HttpPost("add-user")]
         public IActionResult AddUser([FromBody] User user)
         {
             if (user == null)
@@ -25,7 +35,30 @@ namespace Trybe.Tryitter.Controllers
             }
             var add = _usersRepository.AddUser(user);
             return Ok(add);
+        }
 
+        [HttpGet("get-user")]
+        public IActionResult GetUser()
+        {
+            return Ok(_usersRepository.GetUsers());
+        }
+
+        [HttpPut("update-user")]
+        public IActionResult UpdateUser([FromBody] User user)
+        {
+            _usersRepository.UpdateUser(user);
+            return Ok();
+        }
+
+        [HttpDelete("delete-user/{id}")]
+        public IActionResult DeleteUser(int id)
+        {
+            var result = _usersRepository.DeleteUser(id);
+            if (result == false)
+            {
+                return BadRequest();
+            }
+            return Ok();
         }
 
     }
